a5f4c53f65b2eb4a6a0cbca48839f19cf9483232
ajiways <ajiwayswc@gmail.com>
2022-04-13 02:50:56 +0300

    strings to constants in grpc methods :)

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,6 +1,13 @@
 import { Controller } from '@nestjs/common';
 import { GrpcMethod } from '@nestjs/microservices';
 import { AppService } from './app.service';
+import {
+  ADD_ONE,
+  DELETE_ONE,
+  FIND_ALL,
+  PRODUCTS_SERVICE,
+  UPDATE_ONE,
+} from './constants/constants';
 import { AddOneProductRequest } from './contracts/products/AddOneProductRequest';
 import { AddOneProductResponse__Output } from './contracts/products/AddOneProductResponse';
 import { DeleteOneRequest } from './contracts/products/DeleteOneRequest';
@@ -13,22 +20,22 @@ import { UpdateOneResponse__Output } from './contracts/products/UpdateOneRespons
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @GrpcMethod('ProductsService', 'FindAll')
+  @GrpcMethod(PRODUCTS_SERVICE, FIND_ALL)
   findAll(): FindAllProductsResponse__Output {
     return this.appService.findAll();
   }
 
-  @GrpcMethod('ProductsService', 'AddOne')
+  @GrpcMethod(PRODUCTS_SERVICE, ADD_ONE)
   addOne(data: AddOneProductRequest): AddOneProductResponse__Output {
     return this.appService.addOne(data);
   }
 
-  @GrpcMethod('ProductsService', 'DeleteOne')
+  @GrpcMethod(PRODUCTS_SERVICE, DELETE_ONE)
   deleteOne(data: DeleteOneRequest): DeleteOneResponse__Output {
     return this.appService.deleteOne(data);
   }
 
-  @GrpcMethod('ProductsService', 'UpdateOne')
+  @GrpcMethod(PRODUCTS_SERVICE, UPDATE_ONE)
   updateOne(data: UpdateOneRequest): UpdateOneResponse__Output {
     return this.appService.updateOne(data);
   }
---
+++ src/constants/constants.ts
@@ -0,0 +1,5 @@
+export const PRODUCTS_SERVICE = 'ProductsService';
+export const UPDATE_ONE = 'UpdateOne';
+export const DELETE_ONE = 'DeleteOne';
+export const FIND_ALL = 'FindAll';
+export const ADD_ONE = 'AddOne';
