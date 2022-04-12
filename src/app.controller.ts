b813d53c1a01f10f89f5f01210971cf84057e055
ajiways <ajiwayswc@gmail.com>
2022-04-13 02:46:27 +0300

    implemented base CRUD for products and added types generation by @grpc/proto-loader

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,7 +1,35 @@
 import { Controller } from '@nestjs/common';
+import { GrpcMethod } from '@nestjs/microservices';
 import { AppService } from './app.service';
+import { AddOneProductRequest } from './contracts/products/AddOneProductRequest';
+import { AddOneProductResponse__Output } from './contracts/products/AddOneProductResponse';
+import { DeleteOneRequest } from './contracts/products/DeleteOneRequest';
+import { DeleteOneResponse__Output } from './contracts/products/DeleteOneResponse';
+import { FindAllProductsResponse__Output } from './contracts/products/FindAllProductsResponse';
+import { UpdateOneRequest } from './contracts/products/UpdateOneRequest';
+import { UpdateOneResponse__Output } from './contracts/products/UpdateOneResponse';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
+
+  @GrpcMethod('ProductsService', 'FindAll')
+  findAll(): FindAllProductsResponse__Output {
+    return this.appService.findAll();
+  }
+
+  @GrpcMethod('ProductsService', 'AddOne')
+  addOne(data: AddOneProductRequest): AddOneProductResponse__Output {
+    return this.appService.addOne(data);
+  }
+
+  @GrpcMethod('ProductsService', 'DeleteOne')
+  deleteOne(data: DeleteOneRequest): DeleteOneResponse__Output {
+    return this.appService.deleteOne(data);
+  }
+
+  @GrpcMethod('ProductsService', 'UpdateOne')
+  updateOne(data: UpdateOneRequest): UpdateOneResponse__Output {
+    return this.appService.updateOne(data);
+  }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,4 +1,62 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { v4 } from 'uuid';
+import { AddOneProductRequest } from './contracts/products/AddOneProductRequest';
+import { AddOneProductResponse__Output } from './contracts/products/AddOneProductResponse';
+import { DeleteOneRequest } from './contracts/products/DeleteOneRequest';
+import { DeleteOneResponse__Output } from './contracts/products/DeleteOneResponse';
+import { FindAllProductsResponse__Output } from './contracts/products/FindAllProductsResponse';
+import { Product__Output } from './contracts/products/Product';
+import { UpdateOneRequest } from './contracts/products/UpdateOneRequest';
+import { UpdateOneResponse__Output } from './contracts/products/UpdateOneResponse';
 
 @Injectable()
-export class AppService {}
+export class AppService {
+  private mockDBxd: Product__Output[] = [];
+
+  findAll(): FindAllProductsResponse__Output {
+    return { products: this.mockDBxd };
+  }
+
+  addOne({
+    description,
+    name,
+    price,
+  }: AddOneProductRequest): AddOneProductResponse__Output {
+    const product: Product__Output = {
+      id: v4(),
+      description,
+      name,
+      price,
+    };
+
+    this.mockDBxd.push(product);
+
+    return { response: product };
+  }
+
+  deleteOne(data: DeleteOneRequest): DeleteOneResponse__Output {
+    const deleted = this.findOne(data.id);
+
+    this.mockDBxd = this.mockDBxd.filter(
+      (product) => product.id !== deleted.id,
+    );
+
+    return { deleted };
+  }
+
+  updateOne({ body, id }: UpdateOneRequest): UpdateOneResponse__Output {
+    const toUpdate = this.findOne(id);
+
+    if (body.newdesription) toUpdate.description = body.newdesription;
+    if (body.newname) toUpdate.name = body.newname;
+    if (body.newprice) toUpdate.price = body.newprice;
+
+    return { updated: toUpdate };
+  }
+
+  private findOne(id: string): Product__Output {
+    // А как в grpc предусмотреть ошибку, т.е. если ничего не найдется?
+
+    return this.mockDBxd.find((product) => product.id === id);
+  }
+}
